ff0ba2793640f3ba8ef63754513a8a1d40278657
MİZGİN YILDIRAK <mizgin@MIZGIN-MacBook-Air.local>
2023-05-18 20:27:29 +0300

    complete design

---
+++ app.js
@@ -0,0 +1,37 @@
+const arrow = document.getElementById("arrow")
+const listContainer = document.getElementById("list-container")
+const inputBox = document.getElementById("input-box")
+
+arrow.addEventListener("click", () => {
+    if (inputBox.value === '') {
+        alert("You must write something!")
+    } else {
+        const li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li)
+        li.classList.add("check")
+        const span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+
+        span.addEventListener("click", () => {
+            span.parentElement.remove()
+        })
+
+        li.addEventListener('click', function () {
+            if (this.classList.contains('clicked')) {
+                this.classList.remove('clicked')
+                this.style.textDecoration = "none"
+            } else {
+                this.classList.add('clicked')
+                this.style.textDecoration = "line-through"
+            }
+        })
+    }
+
+    inputBox.value = ""
+})
+
+
+
+
