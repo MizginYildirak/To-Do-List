47edd4b25e9a840b06bdaba31a5af85a69ece9f8
MizginYildirak <mizgin_yldrak@hotmail.com>
2023-05-21 14:20:23 +0300

    add local storage

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 const arrow = document.getElementById("arrow")
 const listContainer = document.getElementById("list-container")
 const inputBox = document.getElementById("input-box")
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-arrow.addEventListener("click", () => {
+addList()
+
+arrow.addEventListener("click", (e) => {
     if (inputBox.value === '') {
-        alert("You must write something!")
+        alert("You must write something!");
     } else {
-        spaceBlock()
+        spaceBlock();
+        let inputBoxValue = inputBox.value;
+        saveTasks(inputBoxValue);
+        refreshList(); // Clear the existing task list and add tasks again
 
-        const li = document.createElement("li")
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li)
-        li.classList.add("check")
-        const span = document.createElement("span")
-        span.innerHTML = "\u00d7"
-        li.appendChild(span)
+    }
+    inputBox.value = "";
+});
+
+function refreshList() {
+    // Clear the existing task list
+    listContainer.innerHTML = '';
+    // Add tasks to the list
+    addList();
+}
+
+function addList() {
+    tasks.forEach(item => {
+        const li = document.createElement("li");
+        li.innerHTML = item;
+        listContainer.appendChild(li);
+        li.classList.add("check");
+        const span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
 
         span.addEventListener("click", () => {
-            span.parentElement.remove()
-        })
+            const index = tasks.indexOf(item);
+            span.parentElement.remove();
+            removeTask(index);
+        });
 
         li.addEventListener('click', function () {
             if (this.classList.contains('clicked')) {
@@ -29,10 +50,14 @@ arrow.addEventListener("click", () => {
                 this.style.textDecoration = "line-through"
             }
         })
-    }
+    });
+}
 
-    inputBox.value = ""
-})
+function removeTask(index) {
+    tasks.splice(index, 1)
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 
 function spaceBlock() {
     if (inputBox.selectionStart === 0 && window.event.code === "Space") {
@@ -40,9 +65,13 @@ function spaceBlock() {
     }
 }
 
-inputBox.addEventListener("keyup", function(event) {
+inputBox.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
         arrow.click()
     }
 })
 
+function saveTasks(todo) {
+    tasks.push(todo)
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
